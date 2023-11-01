465440bbb170f9f1761541518304f768f0377ad9
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2023-11-01 13:41:00 -0500

    Defining the data's shape, and the function to re-shape it from the service Next action item: render and display the quizzes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
+
+interface QuizDisplay {
+  quizName: string
+  , quizQuestions: QuestionDisplay[]
+}
+
+interface QuestionDisplay {
+  questionName: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-editor';
+
+  constructor(public quizSvc: QuizService) {  }
+
+  ngOnInit(): void {
+    this.quizzes = this.quizSvc.loadQuizzes().map(x => ({
+      quizName: x.name
+      , quizQuestions: x.questions.map(y => ({
+        questionName: y.name
+      }))
+    }));
+  }
+
+  quizzes: QuizDisplay[] = [];
 }
