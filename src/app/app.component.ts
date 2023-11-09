1450eb6603856ef3d598ae5e81a301f753cdad34
Zenlexi <lang.kaelyn@gmail.com>
2023-11-09 15:27:17 -0600

    Marked for delete working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { QuizService } from './quiz.service';
 interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
+  markedForDelete: boolean;
 }
 
 interface QuestionDisplay {
@@ -27,7 +28,8 @@ export class AppComponent implements OnInit{
       quizName: x.name,
       quizQuestions: x.questions.map((y: any) => ({
         questionName: y.name
-      }))
+      })),
+      markedForDelete: false
     }));
   }
 
@@ -41,7 +43,8 @@ export class AppComponent implements OnInit{
   addNewQuiz = () => {
     let newQuiz: QuizDisplay = {
       quizName: "Untitled Quiz",
-      quizQuestions: []
+      quizQuestions: [],
+      markedForDelete: false
     }
 
     this.quizzes.push(newQuiz);
