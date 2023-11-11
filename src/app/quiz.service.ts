5375ef818a5ab782d23d1b16d78ac57fa2eb964c
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2023-11-11 12:40:25 -0600

    Imported and injected the HttpClient Another step in preparing us for Http calls

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 interface QuizFromWeb {
   name: string;
@@ -12,7 +13,7 @@ interface QuizFromWeb {
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private angularHttpClient: HttpClient) { }
 
   loadQuizzes = () => {
 
