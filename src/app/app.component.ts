9782dd2095fdde531920564f76a3357dc4ae0e1b
Keith <klienert@madisoncollege.edu>
2023-11-15 20:26:58 -0600

    added new material from lectures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+  quizName: string;
+  quizQuestions: QuestionDisplay[];
+  markedForDelete: boolean;
+}
+
+interface QuestionDisplay {
+  questionName: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +20,64 @@ export class AppComponent implements OnInit {
   title = 'quiz-editor';
 
   constructor(
-      public quizzSvc: QuizService
-  ) { }
-
-  ngOnInit(): void {
-      const quizzes = this.quizzSvc.loadQuizzes();
-      console.log(quizzes);
+    public quizSvc: QuizService
+  ) {
   }
+
+  ngOnInit() {
+    const quizzes = this.quizSvc.loadQuizzes();
+    console.log(quizzes);
+
+    this.quizzes = quizzes.map(x => ({
+      quizName: x.name
+      , quizQuestions: x.questions.map((y: any) => ({
+        questionName: y.name
+      }))
+      , markedForDelete: false
+    }));
+
+    console.log(this.quizzes);
+  }
+
+  quizzes: QuizDisplay[] = [];
+
+  selectedQuiz: QuizDisplay | undefined = undefined;
+
+  selectQuiz = (q: QuizDisplay) => {
+    this.selectedQuiz = q;
+    console.log(this.selectedQuiz);
+  };
+
+  addNewQuiz = () => {
+    const newQuiz = {
+      quizName: "Untitled Quiz"
+      , quizQuestions: []
+      , markedForDelete: false
+    };
+
+    this.quizzes = [
+      ...this.quizzes
+      , newQuiz 
+    ];
+
+    this.selectedQuiz = newQuiz;
+  };
+
+  addNewQuestion = () => {
+    
+    if (this.selectedQuiz) {
+      this.selectedQuiz.quizQuestions = [
+        ...this.selectedQuiz.quizQuestions
+        , {
+          questionName: "Untitled Question"
+        }
+      ];
+    }
+  };
+
+  removeQuestion = (questionToRemove: QuestionDisplay) => {
+    if (this.selectedQuiz) {
+      this.selectedQuiz.quizQuestions = this.selectedQuiz.quizQuestions.filter(x => x !== questionToRemove);
+    }
+  };
 }
