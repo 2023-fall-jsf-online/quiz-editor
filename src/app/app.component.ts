9b4974c2bf235ace2e1d5c18ddeb5488ddb801df
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2023-11-22 12:54:27 -0600

    Added quiz loading message.
    Added quiz loading message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,7 @@ export class AppComponent implements OnInit {
   }
 
   errorLoadingQuizzes = false;
+  loadingQuizzes = true
 
   loadQuizzesFromCloud = async () => {
 
@@ -38,11 +39,13 @@ export class AppComponent implements OnInit {
           questionName: y.name
         }))
         , markedForDelete: false
-      }));      
+      }));   
+      this.loadingQuizzes = false;   
     }
     catch (err) {
       console.error(err);
-      this.errorLoadingQuizzes = true;      
+      this.errorLoadingQuizzes = true;  
+      this.loadingQuizzes = false;    
     }
   };
 
