88688f5cc76d46181a880ee3a9a14b0922b80653
Han434 <112228783+Han434@users.noreply.github.com>
2023-11-29 11:52:34 +0630

    Defining, Shapping and Displaying quiz data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { QuizService } from './quiz.service';
+
+interface QuizDisplay {
+  quizzName: string;
+  quizzQuestions: QuestionDisplay[];
+}
+
+interface QuestionDisplay {
+  questionName: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-editor';
+
+  constructor(
+    public quizSvc:QuizService
+  ) {
+
+  }
+
+  ngOnInit() {
+    const quizzes = this.quizSvc.loadQuizes();
+
+    this.quizzes = quizzes.map(x => ({
+      quizzName: x.name
+      , quizzQuestions: x.questions.map((y: any) => ({
+        questionName: y.name
+      }))
+    })
+    )
+
+    console.log(quizzes);
+  }
+
+  quizzes: QuizDisplay[] = [];
 }
