65f58d89c2806e27a9753a10f66103ddde2b98fb
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2023-12-02 16:04:03 -0600

    save buttons hooked up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { QuizService, QuizFromWeb, ShapeForSavingNewQuizzes, ShapeForSavingEditedQuizzes } from './quiz.service';
 import { 
   trigger
   , transition
@@ -228,5 +228,36 @@ export class AppComponent implements OnInit {
 
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
+  };
+
+  saveQuizzes = async () => {
+    try {
+
+      const newQuizzes: ShapeForSavingNewQuizzes[] = this.getAddedQuizzes().map(x =>({
+        quizName: x.quizName
+        , quizQuestions: x.quizQuestions.map(y=> y.questionName)
+
+      }));
+
+
+      const editedQuizzes: ShapeForSavingEditedQuizzes[] = this.getEditedQuizzes().map(x=> ({
+        quiz:  x.quizName
+        , questions: x.quizQuestions.map(y => ({
+          question: y.questionName
+        }))
+      })); 
+
+      const numberOfUpdatedQuizes = await this.quizSvc.saveQuizzes(
+        editedQuizzes
+        , newQuizzes
+
+      );
+
+      console.log("numberOfUpdateQuizzes", numberOfUpdatedQuizes)
+    }
+
+    catch (err) {
+      console.error(err);
+    }
   };
 }
