3c1482d9165c2c5bd0dc6a427e126be632c0edcd
vandoors <143453791+vandoors@users.noreply.github.com>
2023-12-13 14:52:00 -0600

    Improve endpoint error handling
    When error, display error message and do not load normal content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ export class AppComponent implements OnInit {
   ) {
   }
 
+  errorLoadingQuizzes = false;
   ngOnInit() {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
@@ -39,7 +40,8 @@ export class AppComponent implements OnInit {
         }));
       },
       err => {
-        console.log(err);
+        console.log(err.error);
+        this.errorLoadingQuizzes = true;
       }
     );
 
