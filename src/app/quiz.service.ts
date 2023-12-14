98e6295f0244e06d7a6cbb643062b742b3a8b60f
Keith <klienert@madisoncollege.edu>
2023-12-14 10:01:18 -0600

    added error handling, ngIf on top row

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -20,7 +20,7 @@ export class QuizService {
   loadQuizzes = () => {
     // need to make a call to the Http EndPoint (observables)
     const quizzesFromWeb = this.angularHttpClient.get<QuizFromWeb[]>(
-      "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
+      "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name1=Keith%20Lienert"
     );
 
     return quizzesFromWeb;
