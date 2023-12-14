33c4892e3692abff7eb2bcf7f8921b56a5a72c15
jpdhmc <112502289+jpdhmc@users.noreply.github.com>
2023-12-13 22:48:09 -0600

    Savequizzes() now working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { QuizService, QuizFromWeb, ShapeForSavingNewQuizzes, ShapeForSavingEditedQuizzes } from './quiz.service';
 import {
   trigger, transition, animate, keyframes, style
 } from '@angular/animations';
@@ -224,5 +224,29 @@ export class AppComponent implements OnInit {
 
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
+  };
+
+  saveQuizzes = async () => {
+    try {
+      const newQuizzes: ShapeForSavingNewQuizzes[] = this.getAddedQuizzes().map( x => ({
+        quizName: x.quizName
+        , quizQuestions: x.quizQuestions.map(y => y.questionName)
+      }));
+
+      const editedQuizzes: ShapeForSavingEditedQuizzes[] = this.getEditedQuizzes().map(x => ({
+        quiz: x.quizName
+        , questions: x.quizQuestions.map(y => ({
+          question: y.questionName
+        }))
+      }));
+
+      const numberOfUpdatedQuizzes = await this.quizSvc.saveQuizzes(
+        editedQuizzes, newQuizzes
+      );
+      console.log("numberOfUpdatedQuizzes", numberOfUpdatedQuizzes);
+    }
+    catch (err) {
+      console.error(err);
+    }
   };
 }
